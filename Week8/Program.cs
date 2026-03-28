88c7c742a3d783d0d08c91d05ee0b985aafe9ad0
maksymtsutsik56 <maxim.tsytsyk@gmail.com>
2026-03-28 14:21:51 +0200

    Done level 1

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -18,3 +18,37 @@ internal abstract class DeliveryItem
         Console.WriteLine($"{TrackingNumber} - {Weight}");
     }
 }
+
+
+internal class Letter : DeliveryItem
+{
+    public Letter(string trakingNmber, double weight) : base(trakingNmber, weight)
+    {
+    }
+
+    public override double CalculateCost()
+    {
+        return 15 + Weight * 10;
+    }
+}
+
+internal class Parcel : DeliveryItem
+{
+    public string Dimensions { get; set; }
+
+    public Parcel(string trakingNmber, double weight, string dimensions) : base(trakingNmber, weight)
+    {
+        Dimensions = dimensions;
+    }
+
+    public override double CalculateCost()
+    {
+        return 50 + Weight * 25;
+    }
+
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.Write($"Dimensions: {Dimensions}");
+    }
+}
