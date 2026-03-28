13970e1876310fb01b05fe7e96bb11823786a0a5
maksymtsutsik56 <maxim.tsytsyk@gmail.com>
2026-03-28 14:16:24 +0200

    Deliveryitem implemented

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Logistics;
 
-Console.WriteLine("Hello, World!");
+internal abstract class DeliveryItem
+{
+    public double Weight { get; set; }
+    public string TrackingNumber { get; set; }
+
+    protected DeliveryItem(string trakingNumber, double weight)
+    {
+        Weight = weight;
+        TrackingNumber = trakingNumber;
+    }
+
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"{TrackingNumber} - {Weight}");
+    }
+}
